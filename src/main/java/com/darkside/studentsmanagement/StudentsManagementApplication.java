fb37626232b8acd6b548f8672b7083c132be6f7a
Alexandru Smarandache <alex.smarandache@yahoo.com>
2021-03-22 02:17:11 +0200

    Update StudentsManagementApplication.java

--- src/main/java/com/darkside/studentsmanagement/StudentsManagementApplication.java
+++ src/main/java/com/darkside/studentsmanagement/StudentsManagementApplication.java
@@ -1,15 +1,11 @@
 package com.darkside.studentsmanagement;
 
-import com.darkside.studentsmanagement.students.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
+/**
+ * @Author Alex Smarandache
+ */
 @SpringBootApplication
 
 public class StudentsManagementApplication {
